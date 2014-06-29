8a40da16f47f4a2ff7d82d27287424ca1d9687b3
godotcolor <godotcolor@163.com>
2014-06-29 22:23:10 +0800

    test

--- SendEmailActivity/src/cn/com/bcf/sendemailtest/SimpleMailSender.java
+++ SendEmailActivity/src/cn/com/bcf/sendemailtest/SimpleMailSender.java
@@ -12,6 +12,8 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;    
 import javax.mail.internet.MimeMessage;    
 import javax.mail.internet.MimeMultipart;
+
+
 /**   
  * 简单邮件（不带附件的邮件）发送器   
  */    
